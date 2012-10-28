68f9c8cd8d63b7ce692bb5d19586434aaed8499b
Johan Brinch <brinchj@gmail.com>
2012-10-28 10:33:57 +0100

    Add Cabal files

---
+++ Setup.hs
@@ -0,0 +1,10 @@
+#!/usr/bin/env runhaskell
+
+import Control.Monad
+import Distribution.Simple
+import System.Process
+
+main = defaultMainWithHooks simpleUserHooks { postInst = myPostInst }
+
+
+myPostInst args flags desc info = return ()

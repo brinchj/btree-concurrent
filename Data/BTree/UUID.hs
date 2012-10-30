e0d1e06f0625d11c80a075fbbcedd8f6678b1675
Johan Brinch <brinchj@gmail.com>
2012-10-30 12:21:35 +0100

    Refactor for fewer dependencies

--- Data/BTree/UUID.hs
+++ Data/BTree/UUID.hs
@@ -15,14 +15,13 @@ import Control.Concurrent
 import Data.Typeable
 
 import qualified Data.Char as C
-import qualified Crypto.Random.AESCtr as Prng
-import System.Entropy
 
 import Data.Serialize
 
 import System.IO.Unsafe
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Char8      as B
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.ByteString.Base64     as B64
 
 newtype UUID = UUID { unUUID :: B.ByteString }
              deriving (Ord, Eq)
@@ -35,31 +34,16 @@ instance Serialize UUID where
 
 
 instance Show UUID where
-  show (UUID bs) = B.unpack $ B16.encode bs
-
-instance Read UUID where
-  readsPrec _ s' = let s = dropWhile C.isSpace s'
-                       (bs, left) = B16.decode $ B.pack $ take 32 s
-                  in [(UUID bs, drop 32 s) | B.null left]
+  show (UUID bs) = B.unpack $ B64.encode bs
 
 
-
-uuids :: MVar [UUID]
-uuids = unsafePerformIO $ newMVar []
+uuids :: MVar Int
+uuids = unsafePerformIO $ newMVar 1
 {-# NOINLINE uuids #-}
 
 
 uuid :: IO UUID
-uuid =
-  do ls <- takeMVar uuids
-     case ls of
-       (x:xs) -> do putMVar uuids xs
-                    return x
-       [] -> do ent <- getEntropy 64
-                let Right gen = Prng.make ent
-                putMVar uuids $ map UUID $ map fst $
-                  {- always skip first -}
-                  tail $ iterate (\(_, gen') -> Prng.genRandomBytes gen' 16) (B.empty, gen)
-                uuid
+uuid = (UUID . encode) `fmap`
+       (modifyMVar uuids $ \n -> return (n+1, n))
 
 
